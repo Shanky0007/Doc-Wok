ed7b7d5d3010395d6921087c29fd3d5639e3840b
Shanky0007 <sp777216@gmail.com>
2025-09-14 22:59:53 +0530

    update auth backend

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './.env'
+})
+
+import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+import authRoutes from '../routes/auth.route.js';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//middleware add 
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_DB_URI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log('MongoDB Error:', err));
+
+
+//routes
+app.use("/api/auth", authRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
